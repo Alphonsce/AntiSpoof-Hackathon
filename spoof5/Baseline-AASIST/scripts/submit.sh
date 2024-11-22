d9deea38ecee0eb82c1093b679e200935f2472de
Alphonsce <varlamov.al@phystech.edu>
2024-11-23 00:19:56 +0300

    made submission script for SEMAA

---
+++ spoof5/Baseline-AASIST/scripts/submit.sh
@@ -0,0 +1,11 @@
+ckpt_path="exp_result/SEMAA_2021_ep100_bs24_rawboost/weights/epoch_1_0.001.pth"
+
+CUDA_VISIBLE_DEVICES="7"
+
+CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES python submit.py \
+    --architecture SEMAA \
+    --ckpt_path $ckpt_path \
+    --config_path ./config/SEMAA_2021.conf \
+    --device cuda \
+    --output_file semaa_submit_sigmoid.csv \
+    --need_sigmoid
