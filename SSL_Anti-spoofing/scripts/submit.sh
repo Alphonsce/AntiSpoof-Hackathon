91af8cacb1afe102e516cea4b2fa1a6a3c0a15fc
Alphonsce <varlamov.al@phystech.edu>
2024-11-23 10:15:34 +0300

    made some more submissions

--- SSL_Anti-spoofing/scripts/submit.sh
+++ SSL_Anti-spoofing/scripts/submit.sh
@@ -1,4 +1,5 @@
 ckpt_path="models/model_LA_weighted_CCE_100_24_1e-05_2021_wav2vec_unfreeze/last.pth"
+# ckpt_path=models/model_LA_weighted_CCE_100_24_1e-05_2021_dp_hubert_unfreeze/last.pth
 
 CUDA_VISIBLE_DEVICES="7"
 
@@ -6,7 +7,7 @@ CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES python submit.py \
     --architecture wav2vec \
     --ckpt_path $ckpt_path \
     --device cuda \
-    --output_file wav2vec_submit_sigmoid.csv \
+    --output_file wav2vec_10_epochs_la.csv \
     --need_sigmoid \
     --ssl_backbone wav2vec \
     --ssl_behaviour last-layer
--- spoof5/Baseline-AASIST/scripts/submit.sh
+++ spoof5/Baseline-AASIST/scripts/submit.sh
@@ -1,5 +1,7 @@
 # ckpt_path="exp_result/SEMAA_2021_ep100_bs24_rawboost/weights/epoch_1_0.001.pth"
-ckpt_path=exp_result/SEMAA_ep100_bs24_rawboost/weights/epoch_20_0.018.pth
+# ckpt_path=exp_result/SEMAA_ep100_bs24_rawboost/weights/epoch_20_0.018.pth
+
+ckpt_path="exp_result/SEMAA_2021_ep100_bs24_rawboost/weights/epoch_3_0.000.pth"
 
 CUDA_VISIBLE_DEVICES="7"
 
@@ -8,5 +10,17 @@ CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES python submit.py \
     --ckpt_path $ckpt_path \
     --config_path ./config/SEMAA_2021.conf \
     --device cuda \
-    --output_file semaa_submit_2019_no_sigmoid.csv \
+    --output_file semaa2021_rawboost_3_ep.csv \
+    # --need_sigmoid
+
+ckpt_path="exp_result/SEMAA_2021_ep100_bs24_rawboost/weights/epoch_5_0.016.pth"
+
+CUDA_VISIBLE_DEVICES="7"
+
+CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES python submit.py \
+    --architecture SEMAA \
+    --ckpt_path $ckpt_path \
+    --config_path ./config/SEMAA_2021.conf \
+    --device cuda \
+    --output_file semaa2021_rawboost_5_ep.csv \
     # --need_sigmoid
